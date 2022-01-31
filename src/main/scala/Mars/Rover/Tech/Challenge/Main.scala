8438bf78246c2c6b3f1d52ff69390735c04d550c
Ben Mcallister <ben.mcallister@itv.com>
2022-01-31 16:37:00 +0000

    wip

--- src/main/scala/Mars/Rover/Tech/Challenge/Main.scala
+++ src/main/scala/Mars/Rover/Tech/Challenge/Main.scala
@@ -1,16 +1,20 @@
 package Mars.Rover.Tech.Challenge
 
-import Mars.Rover.Tech.Challenge.Commands.doYouWantToMove
 import Mars.Rover.Tech.Challenge.MarsRover.Rover._
 import Mars.Rover.Tech.Challenge.MarsRover._
 
 
 object Main extends App {
-  val xAxisLength = scala.io.StdIn.readLine("please enter an x-axis length:- ")
-  val yAxisLength = scala.io.StdIn.readLine("please enter an y-axis length:- ")
+//  val xAxisLength = scala.io.StdIn.readLine("please enter an x-axis length:- ")
+//  val yAxisLength = scala.io.StdIn.readLine("please enter an y-axis length:- ")
+//
+//  val newMap: MapSize = MapSize(xAxisLength.toInt, yAxisLength.toInt)
+//  val newRover = Rover(newMap)
+//  println(newRover.currentLocationInfo(newMap))
+//  doYouWantToMove(newRover, newMap)
+//  val xAxisLength = scala.io.StdIn.readLine("please enter an x-axis length:- ")
+//  val yAxisLength = scala.io.StdIn.readLine("please enter an y-axis length:- ")
+  val newRover = Rover(MapSize(10, 10))
 
-  val newMap: MapSize = MapSize(xAxisLength.toInt, yAxisLength.toInt)
-  val newRover = Rover(newMap)
-  println(newRover.currentLocationInfo(newMap))
-  doYouWantToMove(newRover, newMap)
+  println(newRover.getFromAToB(newRover.position, CoOrdinate(5, 6)))
 }
--- src/main/scala/Mars/Rover/Tech/Challenge/MarsRover.scala
+++ src/main/scala/Mars/Rover/Tech/Challenge/MarsRover.scala
@@ -6,6 +6,7 @@ object MarsRover {
   final case class Rover(theMap: MapSize, position: CoOrdinate = CoOrdinate(0, 0), heading: Direction = Direction(East)) {
     def location: String = s"${position.location}"
     def direction: String = s"${heading.direction}"
+
     def moveForward: Rover = {
       val newPosition: CoOrdinate = heading.direction match {
         case North => CoOrdinate(position.x, position.y - 1)
@@ -24,18 +25,31 @@ object MarsRover {
       isItTheEndOfTheMap(newPosition)
     }
 
-    def turnRight: Rover = heading match {
+    def turnClockWise: Rover = heading match {
       case Direction(North) => Rover(theMap, position, Direction(East))
       case Direction(East) => Rover(theMap, position, Direction(South))
       case Direction(South) => Rover(theMap, position, Direction(West))
       case Direction(West) => Rover(theMap, position, Direction(North))
     }
 
-    def turnLeft: Rover = heading match {
+    def turnAntiClockWise: Rover = heading match {
       case Direction(North) => Rover(theMap, position, Direction(West))
       case Direction(East) => Rover(theMap, position, Direction(North))
       case Direction(South) => Rover(theMap, position, Direction(East))
       case Direction(West) => Rover(theMap, position, Direction(South))
+    }
+
+    def getFromAToB(a: CoOrdinate, b: CoOrdinate, moves: List[String] = Nil): String = {
+      val movesForward = b.x - a.x
+      val movesDown = b.y - a.y
+      val floor = movesForward + movesDown
+
+      if (a.y == b.y) (b.x - a.x).toString
+      else
+        if (a.x != b.x) (b.y - a.y).toString
+
+      s"$floor moves to location ${b.location}"
+
     }
 
     def currentLocationInfo(newMap: MapSize): String =
